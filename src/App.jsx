9dd7f7f71d136530475c55b98da062e04d06a667
Gokulavasan S <mrideaman03@gmail.com>
2024-08-01 17:28:01 +0530

    preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Header/Navbar';
@@ -9,10 +9,23 @@ import About from './Pages/About/About';
 // import Portfolio from './Pages/Portfolio';
 import Contacts from './Pages/ContactUs/Contact';
 import Footer from './Components/Footer/Footer'
+import Preloader from './Preloader';
 
 function App() {
+const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2500); 
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
+    <div className="App">
+      {loading ? <Preloader setLoading={setLoading} /> : (
     <Router>
+      <Preloader />
       <Navbar />
       <div className="main">
         <Home />
@@ -23,6 +36,8 @@ function App() {
       </div>
       <Footer />
     </Router>
+    )}
+    </div>
   );
 }
 
